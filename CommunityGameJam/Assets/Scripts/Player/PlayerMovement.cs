774f2eefd93649fc77b16153ee130c18fa29ae77
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-08-25 00:42:25 +0300

    Added a player controller

--- CommunityGameJam/Assets/Scripts/Player/PlayerMovement.cs
+++ CommunityGameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,9 +3,58 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 movement;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private GroundCheck groundCheck;
 
-    void Update(){
-        
+    private Vector2 movement;
+    private bool jumping;
+    private bool canJump = true;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        groundCheck.groundHitEvent.AddListener(HitGround);
+    }
+
+    private void OnDisable()
+    {
+        groundCheck.groundHitEvent.RemoveListener(HitGround);
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        if (canJump)
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                jumping = true;
+                canJump = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+
+        if (jumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            jumping = false;
+        }
+    }
+
+    void HitGround(Collider2D collision)
+    {
+        canJump = true;
     }
 }
