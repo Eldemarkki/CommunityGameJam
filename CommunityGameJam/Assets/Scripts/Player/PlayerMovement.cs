e4e36b53de3853c1302f9ffb413153d012a97e37
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-08-24 23:48:50 +0300

    Initial commit

---
+++ CommunityGameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+
+    void Update(){
+        
+    }
+}
