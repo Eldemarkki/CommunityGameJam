f445e1a90d3dd7542a3993c1f2f044e96f45a3a3
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-08-27 20:22:53 +0300

    Added new items

---
+++ CommunityGameJam/Assets/Plant.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            animator.SetTrigger("ShakePlant");
+        }
+    }
+}
