f1c8c945bf81f05a31d5bd9015f7adf944ac41ee
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-08-28 21:36:15 +0300

    Added a menu

---
+++ CommunityGameJam/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private Toggle toggle;
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+        toggle.onValueChanged.AddListener(ToggleChanged);
+    }
+
+    private void ToggleChanged(bool value)
+    {
+        audioSource.mute = !value;
+    }
+}
