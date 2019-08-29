cc5754b7e35cea2d4c77cea397b021a5307730e1
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-08-29 19:30:27 +0300

    Fixed sounds

--- CommunityGameJam/Assets/Scripts/MenuManager.cs
+++ CommunityGameJam/Assets/Scripts/MenuManager.cs
@@ -1,8 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public Toggle musicToggle;
+    public Toggle sfxToggle;
+
+    public static MenuManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume", 0);
+        musicToggle.SetIsOnWithoutNotify(musicVolume == 0);
+        SoundController.instance.SetVolume("musicVolume", musicVolume);
+
+        float sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 0);
+        sfxToggle.SetIsOnWithoutNotify(sfxVolume == 0);
+        SoundController.instance.SetVolume("sfxVolume", sfxVolume);
+    }
+
+    public void FlipMusic(){
+        SoundController.instance.FlipMusic();
+    }
+
+    public void FlipSFX(){
+        SoundController.instance.FlipSFX();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
---
+++ CommunityGameJam/Assets/Scripts/SoundController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    private float originalVolume = -80;
+
+    public static SoundController instance;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void FlipMusic()
+    {
+        float currentVolume;
+        mixer.GetFloat("musicVolume", out currentVolume);
+
+        if (currentVolume == 0)
+            mixer.SetFloat("musicVolume", originalVolume);
+        else
+            mixer.SetFloat("musicVolume", 0);
+
+        mixer.GetFloat("musicVolume", out currentVolume);
+        PlayerPrefs.SetFloat("musicVolume", currentVolume);
+    }
+
+    public void SetVolume(string target, float volume)
+    {
+        mixer.SetFloat(target, volume);
+        PlayerPrefs.SetFloat(target, volume);
+    }
+
+    public void FlipSFX()
+    {
+        float currentVolume;
+        mixer.GetFloat("sfxVolume", out currentVolume);
+
+        if (currentVolume == 0)
+            mixer.SetFloat("sfxVolume", originalVolume);
+        else
+            mixer.SetFloat("sfxVolume", 0);
+
+        mixer.GetFloat("sfxVolume", out currentVolume);
+        PlayerPrefs.SetFloat("sfxVolume", currentVolume);
+    }
+}
